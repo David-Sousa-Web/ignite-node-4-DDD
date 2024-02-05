af1c58dcc90c9ea4c20a486fcfa21216c40640fc
David Sousa <david01sousa@hotmail.com>
2024-02-05 12:29:09 -0300

    feat: mapping props

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,15 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
   title: string;
-  authorId: string;
+  bestAnswerId?: UniqueEntityID;
+  authorId: UniqueEntityID;
+  content: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {}
