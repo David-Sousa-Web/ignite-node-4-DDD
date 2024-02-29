f1f08c64235dac5d5ee90f34a86d9c55eef6e8a7
David Sousa <david01sousa@hotmail.com>
2024-02-28 21:26:20 -0300

    feat: implements clean arquiteture and create question

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,19 @@
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/question-repository'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+}
+
+test('create an question', async () => {
+  const Question = new CreateQuestionUseCase(fakeQuestionRepository)
+
+  const { question } = await Question.execute({
+    authorId: '1',
+    title: 'Nova pergunta',
+    content: 'Conteudo da pergunta',
+  })
+
+  expect(question.id).toBeTruthy()
+})
